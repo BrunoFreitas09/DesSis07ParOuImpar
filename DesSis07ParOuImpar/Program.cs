16b6c7d51df65c5dbba797e16e23de77db1bcc67
Usuario <Usuario@CPD03-M04>
2024-03-26 11:01:50 -0300

    Adicionar arquivos de projeto.

---
+++ DesSis07ParOuImpar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesSis07ParOuImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Par(0) ou Impar(1): ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            int cont = num;
+
+            while (cont <= 20) 
+            {
+                Console.WriteLine(cont);
+                cont+=2;
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
